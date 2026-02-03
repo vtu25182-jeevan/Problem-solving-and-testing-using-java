05f1ede95362eae67ff788aff574cdbd10efb1df
vtu25182-jeevan <vtu25182@veltech.edu.in>
2026-02-03 11:14:21 +0530

    Add files via upload

---
+++ PSTJ-Week 1/Binarysearch.java
@@ -0,0 +1,16 @@
+public class Binarysearch {
+    static int binarySearch(int[] arr, int key) {
+int low = 0, high = arr.length - 1;
+while (low <= high) {
+int mid = low + (high - low) / 2;
+if (arr[mid] == key)
+return mid;
+else if (arr[mid] < key)
+low = mid + 1;
+else
+high = mid - 1;
+}
+return -1;
+}
+    
+}
---
+++ PSTJ-Week 1/possiblepairs.java
@@ -0,0 +1,10 @@
+public class possiblepairs {
+    static void printPairs(int[] arr) {
+for (int i = 0; i < arr.length; i++) {
+for (int j = i + 1; j < arr.length; j++) {
+System.out.println(arr[i] + " " + arr[j]);
+}
+}
+}
+    
+}
